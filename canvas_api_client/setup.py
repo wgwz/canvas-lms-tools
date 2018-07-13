08426f9705fd1d1fedf777616c6f9e55a4e705f2
Angus Grieve-Smith <grvsmth@panix.com>
2018-07-13 15:03:55 -0400

    Bump to 0.2.12

--- canvas_api_client/setup.py
+++ canvas_api_client/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-__version__ = '0.2.11'
+__version__ = '0.2.12'
 
 here = path.abspath(path.dirname(__file__))
 
